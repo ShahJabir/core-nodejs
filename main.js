d45c9b2fc10875b1f54670024f5b61669bb94f8f
ShahJabir <shahjabirofficial@gmail.com>
2026-01-17 00:37:36 +0600

    rewrite program into hello world

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-import http from "node:http";
-import fs from "node:fs";
-
-const server = http.createServer();
-
-server.on("request", (_, response) => {
-  const result = fs.readFileSync("./text.txt");
-  response.setHeader("Content-Type", "text/plain");
-  response.end(result);
-});
-
-server.listen(4080, "127.0.0.1", () => {
-  console.log("server has started on:", server.address());
-});
+console.log("Hello, World!");
